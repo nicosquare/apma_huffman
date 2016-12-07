beeb8ede7141e7fd6f3b8a64b4a55df35c223776
MaDog <nicolas@weavercol.com>
2016-12-07 04:18:43 +0100

    Compression et Decompression à demi faire

--- src_etudiants/tp_huffman.adb
+++ src_etudiants/tp_huffman.adb
@@ -6,13 +6,43 @@ procedure tp_huffman is
 -- COMPRESSION
 ------------------------------------------------------------------------------
 
-	procedure Compresse(Nom_Fichier_In, Nom_Fichier_Out : in String) is
-	A : Arbre_Huffman;
-	begin
-		
-		A := Cree_Huffman(Nom_Fichier_In);
-		Affiche(A);
+	procedure Open_Fichier(Fichier :out Ada.Streams.Stream_IO.File_Type; Flux: out Stream_Access; Nom_Fichier: in String ) is
+    begin
+        Open(Fichier,In_File,Nom_Fichier);
+        Flux := Stream(Fichier);
+    end Open_fichier;
 
+	procedure Compresse(Nom_Fichier_In, Nom_Fichier_Out : in String) is
+	Fichier_In : Ada.Streams.Stream_IO.File_Type;
+	Fichier_Out : Ada.Streams.Stream_IO.File_Type;
+	Flux_In: Stream_Access;
+	Flux_Out: Stream_Access;
+	H : Arbre_Huffman;
+	D : Dico_Caracteres;
+    C : Character;
+    Cod : Code_Binaire;
+    I : Integer;
+	begin
+
+		Create(Fichier_Out,Out_File,Nom_Fichier_Out);
+        Flux_Out := Stream(Fichier_Out);
+		
+		H := Cree_Huffman(Nom_Fichier_In);		
+		D := Genere_Dictionnaire(H);
+
+		I := Ecrit_Huffman(H,Flux_Out);
+
+		Affiche(D);
+
+		Open_Fichier(Fichier_In,Flux_In,Nom_Fichier_In);
+
+		while not End_Of_File(Fichier_In) loop
+            Character'Read(Flux_In,C);
+            -- ??
+        end loop;
+
+        Close(Fichier_In);
+        Close(Fichier_Out);
 
 		return;
 	end Compresse;
@@ -24,8 +54,31 @@ procedure tp_huffman is
 ------------------------------------------------------------------------------
 
 	procedure Decompresse(Nom_Fichier_In, Nom_Fichier_Out : in String) is
+	
+	Fichier_In : Ada.Streams.Stream_IO.File_Type;
+	Fichier_Out : Ada.Streams.Stream_IO.File_Type;
+	Flux_In: Stream_Access;
+	Flux_Out: Stream_Access;
+	H : Arbre_Huffman;
+	D : Dico_Caracteres;
+    C : Character;
+    Cod : Code_Binaire;
+    O : Octet;
 	begin
-		-- A COMPLETER!
+		
+		Create(Fichier_Out,Out_File,Nom_Fichier_Out);
+		Flux_Out := Stream(Fichier_Out);
+
+		Open_Fichier(Fichier_In,Flux_In,Nom_Fichier_In);
+
+		H := Lit_Huffman(Flux_In);
+		D := Genere_Dictionnaire(H);
+
+		while not End_Of_File(Fichier_In) loop
+            Octet'Read(Flux_In,O);
+        	-- ??    
+        end loop;
+
 		return;
 	end Decompresse;
 
